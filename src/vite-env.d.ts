e607d2b8fc4a74d96e6cf891b9c030f929e52bd0
Caio Vinícius <caio1525pereira@gmail.com>
2024-03-21 21:46:14 -0300

    chore: :label: Tipagem das .envs

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -1,1 +1,10 @@
 /// <reference types="vite/client" />
+
+interface ImportMetaEnv {
+  readonly VITE_API_URL: string;
+  readonly VITE_ENABLE_API_DELAY: boolean;
+}
+
+interface ImportMeta {
+  readonly env: ImportMetaEnv;
+}
