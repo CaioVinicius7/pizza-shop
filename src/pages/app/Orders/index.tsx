bc86b09fb3be71a741233534fb973d1638093b29
Caio Vinícius <caio1525pereira@gmail.com>
2024-03-29 17:41:53 -0300

    feat: :children_crossing: Loading dos pedidos

--- src/pages/app/Orders/index.tsx
+++ src/pages/app/Orders/index.tsx
@@ -13,6 +13,7 @@ import {
   TableRow
 } from "@/components/ui/table";
 
+import { OrdersTableSkeleton } from "./OrdersTableSkeleton";
 import { OrderTableFilters } from "./OrderTableFilters";
 import { OrderTableRow } from "./OrderTableRow";
 
@@ -28,7 +29,7 @@ export function Orders() {
     .transform((page) => page - 1)
     .parse(searchParams.get("page") ?? "1");
 
-  const { data: result } = useQuery({
+  const { data: result, isLoading: isLoadingOrders } = useQuery({
     queryKey: ["orders", pageIndex, orderId, customerName, status],
     queryFn: () =>
       getOrders({
@@ -77,6 +78,8 @@ export function Orders() {
                   result.orders.map((order) => (
                     <OrderTableRow key={order.orderId} order={order} />
                   ))}
+
+                {isLoadingOrders && <OrdersTableSkeleton />}
               </TableBody>
             </Table>
           </div>
