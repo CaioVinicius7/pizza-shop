26fb4cf83612abe5b5bbeecf7688bf64734694cf
Caio Vinícius <caio1525pereira@gmail.com>
2024-04-24 18:11:21 -0300

    test: :white_check_mark: Teste E2E das métricas do dashboard

---
+++ test/dashboard.e2e-spec.ts
@@ -0,0 +1,33 @@
+import { expect, test } from "@playwright/test";
+
+test.describe("Dashboard", () => {
+  test.beforeEach(async ({ page }) => {
+    await page.goto("/", {
+      waitUntil: "networkidle"
+    });
+  });
+
+  test.afterEach(async ({ page }) => {
+    await page.waitForTimeout(2000);
+  });
+
+  test("Display day orders amount metric in successfully", async ({ page }) => {
+    expect(page.getByText("20", { exact: true })).toBeVisible();
+    expect(page.getByText("-5% em relação a ontem")).toBeVisible();
+  });
+
+  test("Display month orders amount metric", async ({ page }) => {
+    expect(page.getByText("200", { exact: true })).toBeVisible();
+    expect(page.getByText("+7% em relação ao mês passado")).toBeVisible();
+  });
+
+  test("Display month canceled orders amount metric", async ({ page }) => {
+    expect(page.getByText("5", { exact: true })).toBeVisible();
+    expect(page.getByText("-5% em relação ao mês passado")).toBeVisible();
+  });
+
+  test("Display month revenue metric", async ({ page }) => {
+    expect(page.getByText("R$ 200,00")).toBeVisible();
+    expect(page.getByText("+10% em relação ao mês passado")).toBeVisible();
+  });
+});
