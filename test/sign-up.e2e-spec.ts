650697cdfee6bc0aa4d207fef869d79e264d2b0e
Caio Vinícius <caio1525pereira@gmail.com>
2024-04-24 17:29:39 -0300

    test: :white_check_mark: Testes E2E de cadastro

---
+++ test/sign-up.e2e-spec.ts
@@ -0,0 +1,45 @@
+import { expect, test } from "@playwright/test";
+
+test.describe("Sign up", () => {
+  test.beforeEach(async ({ page }) => {
+    await page.goto("/sign-up", {
+      waitUntil: "networkidle"
+    });
+  });
+
+  test.afterEach(async ({ page }) => {
+    await page.waitForTimeout(2000);
+  });
+
+  test("Sign up successfully", async ({ page }) => {
+    await page.getByLabel("Nome do estabelecimento").fill("Pizza Shop");
+    await page.getByLabel("Seu nome").fill("John Doe");
+    await page.getByLabel("Seu celular").fill("(35) 9 9999-9999");
+    await page.getByLabel("Seu e-mail").fill("johndoe@example.com");
+
+    await page.getByRole("button", { name: "Finalizar cadastro" }).click();
+
+    const toast = page.getByText("Restaurante cadastrado com sucesso!");
+
+    expect(toast).toBeVisible();
+  });
+
+  test("Sign up with error", async ({ page }) => {
+    await page.getByLabel("Nome do estabelecimento").fill("Invalid name");
+    await page.getByLabel("Seu nome").fill("John Doe");
+    await page.getByLabel("Seu celular").fill("(35) 9 9999-9999");
+    await page.getByLabel("Seu e-mail").fill("johndoe@example.com");
+
+    await page.getByRole("button", { name: "Finalizar cadastro" }).click();
+
+    const toast = page.getByText("Erro ao cadastrar o restaurante.");
+
+    expect(toast).toBeVisible();
+  });
+
+  test("Navigate to login restaurant page", async ({ page }) => {
+    await page.getByRole("link", { name: "Fazer login" }).click();
+
+    expect(page.url()).toContain("/sign-in");
+  });
+});
